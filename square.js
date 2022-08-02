26126d37c17ebb4e291636f3d408612da28047de
Eacarr1992 <90227363+Eacarr1992@users.noreply.github.com>
2022-08-02 14:53:45 -0500

    Add files via upload

---
+++ square.js
@@ -0,0 +1,8 @@
+//Get an input Number
+var inputNum = prompt ("Type a number between 1 and 10");
+
+//if valid, display its square
+if (inputNum >= 1 && inputNum <= 10)
+	{document.write("The square of " + inputNum + " is " + inputNum*inputNum);}
+else 
+	{document.write("Invalid number!");}
